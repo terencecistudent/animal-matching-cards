8d9c6a0164b5dfcbac6a9fc18cacd813a555dd6a
terencecistudent <terencelogue100@hotmail.co.uk>
2019-11-10 14:03:24 +0000

    Selected cards test complete

--- testing/scripts/card.js
+++ testing/scripts/card.js
@@ -1,3 +1,34 @@
-function result() {
-    return true;
+/*---------------------------------function for selectedCard----*/
+function selectedCard(card) {
+    if (card === "bee") {
+        return ("Matches!");
+    } else if (card === "crab") {
+        return("Matches!");
+    } else if (card === "fish") {
+        return("Matches!");
+    } else if (card === "koala") {
+        return("Matches!");
+    } else if (card === "squirrel") {
+        return("Matches!");
+    } else if (card === "turtle") {
+        return("Matches!");
+    };
+}
+
+
+/*---------------------------------function for matchedCards----*/
+function matchedCards(card1, card2) {
+    if (card1 === "bee" && card2 === "bee") {
+        return ("Matched!");
+    } else if (card1 === "crab" && card2 === "crab") {
+        return ("Matched!");
+    } else if (card1 === "fish" && card2 === "fish") {
+        return ("Matched!");
+    } else if (card1 === "koala" && card2 === "koala") {
+        return ("Matched!");
+    } else if (card1 === "squirrel" && card2 === "squirrel") {
+        return ("Matched!");
+    } else if (card1 === "turtle" && card2 === "turtle") {
+        return ("Matched!");
+    };
 }
--- testing/spec/cardSpec.js
+++ testing/spec/cardSpec.js
@@ -1,36 +1,90 @@
-
 describe("Matching Card Game", function() {
 
-    /*--------------------------------------Cards----*/
+    /*--------------------------------------Checks the selected card and returns Matches!----*/
+    beforeEach(function() {
+            SelectedCards = new selectedCard;
+    });
 
-    describe("Cards", function() {
-        // Memory Game and Cards should be visible
-        it("memory game container should be visible", function() {
-            expect($(".memory-game")).toBeVisible();
+    describe("Check Cards", function() {
+
+        it("should return Matches! for selecting bee card", function() {
+            var selected = selectedCard("bee");
+            expect(selected).toBe("Matches!");
         });
-        it("memory game container should be visible", function() {
-            expect($(".memory-card")).toBeVisible();
+
+        it("should return Matches! for selecting crab card", function() {
+            var selected = selectedCard("crab");
+            expect(selected).toBe("Matches!");
+        });
+
+        it("should return Matches! for selecting fish card", function() {
+            var selected = selectedCard("fish");
+            expect(selected).toBe("Matches!");
+        });
+
+        it("should return Matches! for selecting koala card", function() {
+            var selected = selectedCard("koala");
+            expect(selected).toBe("Matches!");
+        });
+
+        it("should return Matches! for selecting squirrel card", function() {
+            var selected = selectedCard("squirrel");
+            expect(selected).toBe("Matches!");
+        });
+
+        it("should return Matches! for selecting turtle card", function() {
+            var selected = selectedCard("turtle");
+            expect(selected).toBe("Matches!");
         });
     });
 
 
-    /*--------------------------------------Modal----*/
-
-    describe("Modal", function() {
-        it("should be hidden until called upon", function() {
-            expect($("#myModal")).toBeHidden();
-        });
+    /*------------------------------------Checks the matched cards and returns Matched!----*/
+    beforeEach(function() {
+        MatchedCards = new matchedCards;
     });
 
+    describe("Cards Matched", function() {
+        it("should return Matched! for matched bee cards", function() {
+            var card1 = matchedCards("bee");
+            var card2 = matchedCards("bee");
 
-    /*--------------------------------------Reset Button----*/
-    describe("Check New Game Button", function() {
-        beforeEach(function() {
-            btn = $("#restart");
+            expect(card1, card2).toBe("Matched!");
         });
 
-        it("should allow user to play again", function() {
-            expect(btn).toHaveClass(".btn-lg");
+        it("should return Matched! for matched bee cards", function() {
+            var card1 = matchedCards("crab");
+            var card2 = matchedCards("crab");
+
+            expect(card1, card2).toBe("Matched!");
+        });
+
+        it("should return Matched! for matched bee cards", function() {
+            var card1 = matchedCards("fish");
+            var card2 = matchedCards("fish");
+
+            expect(card1, card2).toBe("Matched!");
+        });
+
+        it("should return Matched! for matched bee cards", function() {
+            var card1 = matchedCards("koala");
+            var card2 = matchedCards("koala");
+
+            expect(card1, card2).toBe("Matched!");
+        });
+
+        it("should return Matched! for matched bee cards", function() {
+            var card1 = matchedCards("squirrel");
+            var card2 = matchedCards("squirrel");
+
+            expect(card1, card2).toBe("Matched!");
+        });
+
+        it("should return Matched! for matched bee cards", function() {
+            var card1 = matchedCards("turtle");
+            var card2 = matchedCards("turtle");
+
+            expect(card1, card2).toBe("Matched!");
         });
     });
 });
